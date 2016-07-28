d57170dd83a2416388234355e85b10c3d9665c62
Alexandr Nikitin <aanikitin@users.noreply.github.com>
2016-07-29 01:45:45 +0700

    mex lsm3d test

--- wrappers/matlab/OpenST_LSM3D.m
+++ wrappers/matlab/OpenST_LSM3D.m
@@ -1,7 +1,33 @@
-function [U,c,it] = OpenST_LSM3D(V,SRC,H,EPS,MAX_ITER)
+function [U,varargout] = OpenST_LSM3D(V,SRC,varargin)
+
+if nargin > 2
+    H = varargin{1};
+else
+    H = [1, 1, 1];
+end;
+
+if nargin > 3
+    EPS = varargin{2};
+else
+    EPS = 0.01 * min(H(:)) / max(V(:));
+end;
+
+if nargin > 4
+    MAX_ITER = varargin{3};
+else
+    MAX_ITER = 100;
+end;
 
 V = permute(V, [3 2 1]);
 [U,c,it] = OpenST_LSM3D_mex(V,SRC,H,EPS,MAX_ITER);
 U = permute(U, [3 2 1]);
+
+if nargout > 1
+    varargout{1} = c;
+end;
+
+if nargout > 2
+    varargout{2} = it;
+end;
 
 end
---
+++ wrappers/matlab/OpenST_LSM3D_TEST.m
@@ -0,0 +1,46 @@
+function OpenST_LSM3D_TEST(NI, NJ, NK)
+
+H(1) = 1.0 / (NI - 1);
+H(2) = 1.0 / (NJ - 1);
+H(3) = 1.0 / (NK - 1);
+SRC = [.5 .5 .5];
+
+V = ones(NI, NJ, NK);
+
+EPS = 0.01 * max(H(:)) / max(V(:));
+MAX_ITER = 10;
+
+tic;[U,c,it] = OpenST_LSM3D(V,SRC,H,EPS,MAX_ITER);t = toc;
+fprintf('EIKONAL_EX1 test for OpenST_LSM3D MEX\n');
+fprintf('converged: %i; iterations: %i; seconds: %.5f\n',c,it,t);
+[L1, L2, LINF, UMIN, UMEAN, UMAX] = ex_check(U,SRC,H);
+fprintf('L1: %e; L2: %e; LINF: %e; UMIN: %e; UMEAN: %e; UMAX: %e\n', ...
+L1, L2, LINF, UMIN, UMEAN, UMAX);
+end
+
+function [L1, L2, LINF, UMIN, UMEAN, UMAX] = ex_check(U,SRC,H)
+NI = size(U,1);
+NJ = size(U,2);
+NK = size(U,3);
+NN = NI * NJ * NK;
+
+UEXACT = zeros(NI,NJ,NK);
+for i = 0 : (NI - 1)
+    for j = 0 : (NJ - 1)
+        for k = 0 : (NK - 1)
+            di = SRC(1) - i * H(1);
+            dj = SRC(2) - j * H(2);
+            dk = SRC(3) - k * H(3);
+            UEXACT(i + 1,j + 1,k + 1) = sqrt(di^2 + dj ^2 + dk^2);
+        end;
+    end;
+end;
+
+L1 = sum(abs(U(:) - UEXACT(:))) / NN;
+L2 = sum(abs(U(:) - UEXACT(:)).^2) / NN;
+LINF = max(abs(U(:) - UEXACT(:)));
+
+UMIN = min(U(:));
+UMAX = max(U(:));
+UMEAN = mean(U(:));
+end
